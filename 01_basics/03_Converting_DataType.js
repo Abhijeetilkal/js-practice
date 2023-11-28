012e55c58f63a0f6d44fd74817bb4e797159b7b7
Abhijeetilkal <92844199+Abhijeetilkal@users.noreply.github.com>
2023-11-28 10:20:01 +0000

    container config done

---
+++ 01_basics/03_Converting_DataType.js
@@ -0,0 +1,22 @@
+// CONVERTING DATATYPES 
+
+
+// 1.CONVERTING FROM NUMBER TO STRING
+// Method 1
+let score=33;
+let newScore=String(score);
+console.log(typeof newscore); 
+
+// Method 2
+let age=22+"";
+console.log(typeof age);
+
+// 2.CONVERTING FROM STRING TO NUMBER 
+// Method 1
+let Pincode="585105"
+let NewPincode =Number(Pincode);
+console.log(typeof NewPincode);
+
+// Method 2
+let PhoneNumber=+"8884972680";
+console.log(typeof PhoneNumber);
